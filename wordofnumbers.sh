d73767cfff38d192e85ccfd693df77879e61fac6
Otieno27 <86370623+Otieno27@users.noreply.github.com>
2022-05-18 06:57:29 +0000

    Add files via upload

---
+++ wordofnumbers.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#github.com/Otieno27
+
+read x
+read y
+echo $((x + y))
+echo $((x - y))
+echo $((x * y))
+echo $((x / y))
+
+
+#read 10
+#read 4
+#echo $((10 + 4))
+#echo $((10 - 4))
+#echo $((10 * 4))
+#echo $((10 / 4))
